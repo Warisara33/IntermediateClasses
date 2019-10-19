ff59a5e66b8b3699bef9a685fa0af5b28ab5b862
Warisara33 <warisara.sa@rmuti.ac.th>
2019-10-20 01:43:04 +0700

    3_3_UpdateInstaller

--- 3Association_between_Classes/3_3_Composition/Instaler.cs
+++ 3Association_between_Classes/3_3_Composition/Instaler.cs
@@ -2,6 +2,14 @@
 {
     public class Instaler
     {
-
+        private readonly Logger _logger;
+        public Installer(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Install()
+        {
+            _logger.Log("We are installing the cpplication.");
+        }
     }
 }
